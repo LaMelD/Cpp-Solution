0de0386aaf9251d4b1d1e2038d87058d9d7f96fd
LaMelD <54210310+LaMelD@users.noreply.github.com>
2019-09-04 16:33:34 +0900

    Update 시저 암호.cpp

--- Lv1/시저 암호.cpp
+++ Lv1/시저 암호.cpp
@@ -5,9 +5,8 @@ using namespace std;
 
 //ascii 에서 대문자 < 소문자
 string solution(string s, int n) {
-    string answer = "";
-    
-    for (int i = 0; i < s.size(); i++){
+    for (int i = 0; i < s.size(); i++)
+    {
         //공백 판별
         if (s[i] == ' ') continue;
         //소문자 판별
@@ -17,12 +16,12 @@ string solution(string s, int n) {
             else s[i] += n;
         }
         //대문자 판별
-        else if (s[i] >= 'A'){
+        else if (s[i] >= 'A')
+        {
             if (s[i] + n > 'Z')
                 s[i] = s[i] + n - 'Z' + 'A' - 1;
             else s[i] += n;
         }
     }
-    
     return s;
 }
