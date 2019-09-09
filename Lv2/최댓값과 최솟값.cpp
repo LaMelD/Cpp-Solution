1ad882b6de4cc8972e33e9b95f93be8da9d69682
LaMelD <54210310+LaMelD@users.noreply.github.com>
2019-09-09 14:04:07 +0900

    Update 최댓값과 최솟값.cpp

--- Lv2/최댓값과 최솟값.cpp
+++ Lv2/최댓값과 최솟값.cpp
@@ -4,30 +4,38 @@
 
 using namespace std;
 
-string solution(string s) {
+string solution(string s) 
+{
 	string ans = "";
 	int maxi;
 	int mini;
 	int tmp;
 	string s_tmp;
-	for (int i = 0; i < s.length(); i++) {
+	for (int i = 0; i < s.length(); i++) 
+	{
 		s_tmp = "";
 		int j;
 		if (s[i] == ' ') continue;
 
 		//'-'로 시작할때 숫자 추출
-		if (s[i] == '-') {
+		if (s[i] == '-') 
+		{
 			i++;
-			for (j = i; j < s.length(); j++) {
+			for (j = i; j < s.length(); j++) 
+			{
 				if (s[j] == ' ') break;
+				
 				s_tmp += s[j];
 			}
 			tmp = stoi(s_tmp) * -1;
 		}
 		//양수일 때 숫자 추출
-		else {
-			for (j = i; j < s.length(); j++) {
+		else 
+		{
+			for (j = i; j < s.length(); j++) 
+			{
 				if (s[j] == ' ') break;
+				
 				s_tmp += s[j];
 			}
 			tmp = stoi(s_tmp);
@@ -35,14 +43,17 @@ string solution(string s) {
 
 		//첫 maxi, mini 설정
 		//i가 0 또는 1이 되는 순간은 한번이다
-		if (i == 0 || i == 1) {
+		if (i == 0 || i == 1) 
+		{
 			maxi = tmp;
 			mini = tmp;
 		}
-		else {
+		else 
+		{
 			if (maxi < tmp) maxi = tmp;
 			if (mini > tmp) mini = tmp;
 		}
+		
 		i = j;
 	}
 	ans += to_string(mini);
