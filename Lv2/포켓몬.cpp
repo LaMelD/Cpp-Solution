6b7beeccb6ab5214a2ae49dafab6ddadca2fd267
LaMelD <54210310+LaMelD@users.noreply.github.com>
2019-09-09 13:52:33 +0900

    Update 포켓몬.cpp

--- Lv2/포켓몬.cpp
+++ Lv2/포켓몬.cpp
@@ -3,27 +3,30 @@
 
 using namespace std;
 
-int solution(vector<int> nums){
-    sort(nums.begin(), nums.end());
-    //nums.size()의 반을 얻을 수 있다
-    int gett = nums.size() / 2;
-    vector<int> v;
-    v.push_back(nums[0]);
-    int idx = 0;
-    int kind = 1;
-    for (int i = 1; i < nums.size(); i++){
-        //같은 종류면 continue
-        if (v[idx] == nums[i]) continue;
+int solution(vector<int> nums)
+{
+	sort(nums.begin(), nums.end());
+	//nums.size()의 반을 얻을 수 있다
+	int gett = nums.size() / 2;
+	vector<int> v;
+	v.push_back(nums[0]);
+	int idx = 0;
+	int kind = 1;
+    
+	for (int i = 1; i < nums.size(); i++)
+	{
+		//같은 종류면 continue
+		if (v[idx] == nums[i]) continue;
         
-        //다른 종류를 만나면 넣어주고 idx++, kind++
-        v.push_back(nums[i]);
-        idx++;
-        kind++;
-    }
+		//다른 종류를 만나면 넣어주고 idx++, kind++
+		v.push_back(nums[i]);
+		idx++;
+		kind++;
+	}
+	
+	//얻을 수 있는 포켓몬보다 종류가 많다면 얻을 수 있는 포켓몬의 수를 return
+	if (kind > gett) return gett;
     
-    //얻을 수 있는 포켓몬보다 종류가 많다면 얻을 수 있는 포켓몬의 수를 return
-    if (kind > gett) return gett;
-    
-    //아니라면 종류를 리턴
-	  return kind;
+	//아니라면 종류를 리턴
+	return kind;
 }
