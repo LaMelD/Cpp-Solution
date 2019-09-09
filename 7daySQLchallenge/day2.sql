5b73ee6b31e66dfa43e4a4df6ba735dc265f80d9
LaMelD <54210310+LaMelD@users.noreply.github.com>
2019-09-09 13:15:58 +0900

    Update day2.sql

--- 7daySQLchallenge/day2.sql
+++ 7daySQLchallenge/day2.sql
@@ -1,10 +1,12 @@
-동물 보호소에 들어온 동물 중 아픈 동물1의 아이디와 이름을 조회하는 SQL 문을 작성해주세요. 이때 결과는 아이디 순으로 조회해주세요.
+-- 동물 보호소에 들어온 동물 중 아픈 동물1의 아이디와 이름을 조회하는 SQL 문을 작성해주세요. 
+-- 이때 결과는 아이디 순으로 조회해주세요.
 SELECT A.ANIMAL_ID, A.NAME
 FROM ANIMAL_INS A
 WHERE A.INTAKE_CONDITION = "SICK"
 ORDER BY A.ANIMAL_ID ASC;
 
-동물 보호소에 들어온 동물 중 젊은 동물1의 아이디와 이름을 조회하는 SQL 문을 작성해주세요. 이때 결과는 아이디 순으로 조회해주세요.
+-- 동물 보호소에 들어온 동물 중 젊은 동물1의 아이디와 이름을 조회하는 SQL 문을 작성해주세요. 
+-- 이때 결과는 아이디 순으로 조회해주세요.
 SELECT A.ANIMAL_ID, A.NAME
 FROM ANIMAL_INS A
 WHERE A.INTAKE_CONDITION != "Aged"
