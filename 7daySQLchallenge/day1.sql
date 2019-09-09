3e76524cab4510933d271455614d06357c7fc909
LaMelD <54210310+LaMelD@users.noreply.github.com>
2019-09-09 13:16:23 +0900

    Update day1.sql

--- 7daySQLchallenge/day1.sql
+++ 7daySQLchallenge/day1.sql
@@ -1,10 +1,10 @@
-동물 보호소에 들어온 모든 동물의 정보를 ANIMAL_ID순으로 조회하는 SQL문을 작성
+-- 동물 보호소에 들어온 모든 동물의 정보를 ANIMAL_ID순으로 조회하는 SQL문을 작성
 SELECT *
 FROM ANIMAL_INS A
 ORDER BY A.ANIMAL_ID ASC;
 
-동물 보호소에 들어온 모든 동물의 이름과 보호 시작일을 조회하는 SQL문을 작성
-결과는 ANIMAL_ID 역순
+-- 동물 보호소에 들어온 모든 동물의 이름과 보호 시작일을 조회하는 SQL문을 작성
+-- 결과는 ANIMAL_ID 역순
 SELECT A.NAME, A.DATETIME
 FROM ANIMAL_INS A
 ORDER BY A.ANIMAL_ID DESC;
