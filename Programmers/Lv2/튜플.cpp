0752f461de6c713c9d4bf26aedfe18bb9a6614eb
lameld <wqe014@gmail.com>
2020-07-06 02:11:43 +0900

    modified

--- Programmers/Lv2/튜플.cpp
+++ Programmers/Lv2/튜플.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+// 문자 c를 기준으로 문자열 s를 분할한다.
 vector<string> split(string s, char c)
 {
 	vector<string> ret;
@@ -26,6 +27,7 @@ vector<string> split(string s, char c)
 	return ret;
 }
 
+// 문자열 s에서 from 문자를 to 문자로 교체한다.
 string replace(string s, char from, char to)
 {
 	for (int i = 0; i < s.length(); i++)
@@ -38,6 +40,7 @@ string replace(string s, char from, char to)
 	return s;
 }
 
+// 문자열 s에서 왼쪽의 공백을 제거한다.
 string lstrip(string s)
 {
 	string ret;
@@ -74,10 +77,12 @@ vector<int> solution(string s)
 		number_set.push_back(split(str, ' '));
 	}
 
+    // 사이즈가 작은 것부터 오름차순으로 정렬한다.
 	sort(number_set.begin(), number_set.end(), [](vector<string> a, vector<string> b) {return a.size() < b.size(); });
 
 	map<string, int> m;
 
+    // 순서대로 중복되지 않는 것을 넣어준다.
 	for (int i = 0; i < number_set.size(); i++)
 	{
 		for (int j = 0; j < number_set[i].size(); j++)
